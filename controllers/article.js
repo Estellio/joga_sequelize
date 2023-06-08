882ddf8c74b1741ed256fe45d653bd10837141b7
Estellio <pirko.vichmann@voco.ee>
2023-06-08 16:01:22 +0300

    Got article author data

--- controllers/article.js
+++ controllers/article.js
@@ -23,7 +23,14 @@ const getArticleBySlug = (req, res) => {
         },
         include: [{
             model: models.Author,
-        }],
+        },
+            {
+                model: models.Tags,
+                through: {
+                    model: models.ArticleTags
+                }
+            }
+        ],
     })
         .then(article => {
             console.log(article)
@@ -34,7 +41,22 @@ const getArticleBySlug = (req, res) => {
         })
 }
 
+const getArticleByAuthor = (req,res) =>{
+    models.Article.findAll({
+        where: {
+            author_id:req.params.author_id
+        },
+    })
+        .then(article =>{
+            console.log(article)
+            return res.status(200).json({article});
+        })
+        .catch(error=>{
+            return res.status(500).send(error.message);
+        })
+}
+
 module.exports = {
-    getAllArticles,
-    getArticleBySlug
+    getArticleBySlug,
+    getArticleByAuthor
 };
